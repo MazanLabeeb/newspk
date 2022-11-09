92a449f57a1c77fece2a79d43e645f24800aca8c
ItsNyoty <58821897+ItsNyoty@users.noreply.github.com>
2022-11-09 16:59:31 +0100

    added dutch

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ const newspk = require("newspk");
     let limit = 5;              // max-allowed: 15
     let lang = "english";       // for english
     // let lang = "urdu";       // for urdu
+    // let lang = "dutch";      // for dutch
 
     let news = await newspk.news(limit, lang);
     console.log(news)           // An array of object with properties "title", "thumbnail", "body", "created_at", "unique id"
